3a804515a2ec5ca7db6629e58b629532a9ccb0fd
LuisAxel <54085467+LuisAxel@users.noreply.github.com>
2021-12-08 14:06:22 -0600

    Progreso inicial proyecto 2
    Avance:
        Lectura y almacenamiento de entrada
        Despliegue de mensaje error
        Lectura de cadena "OK" entre entradas
    
    Comprobación de errores:
         Entrada no es dígito ni romano
         Entrada es dígito y romano
         Entrada de tipo dígito supera los 4 caracteres
         Entrada de tipo romano supera los 21 caracteres (numero romano más largo = MMMMMMMMMDCCCLXXXVIII

--- Proyecto2.asm
+++ Proyecto2.asm
@@ -1,4 +1,4 @@
-* PROGRAMA START
+* PROGRAMA ROMANO
 
 * DECLARACION CONSTANTES
 
@@ -20,71 +20,50 @@ U1    EQU $0001
 U2    EQU $0002
 U3    EQU $0003
 U4    EQU $0004
+CONT  EQU $0005
+DUMP  EQU $0050
+
+***********************************
+* MAIN
+***********************************
 
       ORG $8000
-INICIO
       LDS #$00FF
       JSR SERIAL
 
-
+START
       CLR U1
       CLR U2
       CLR U3
       CLR U4
-CICLATE
-      LDAA #'?
-      STAA ORDEN
-CICLO
-      LDAA ORDEN
-      CMPA #'?
-      BEQ  CICLO
+      CLR CONT
+      LDX DUMP
 
-      LDAB U1
-      BNE  SIGUET
-BORRA
-      CLR U1
-      CLR U2
-      CLR U3
-      CLR U4
+      JSR ENTRADA 
 
-      CMPA #'S
-      BNE CICLATE
-      STAA U1     * VALIDA S
-      JMP CICLATE
-
-SIGUET
+      LDAA U1
       LDAB U2
-      BNE  SIGUEA
-      CMPA #'T
-      BNE BORRA
-      STAA U2     * VALIDA T
-      JMP CICLATE
-
-SIGUEA
-      LDAB U3
-      BNE  SIGUER
-      CMPA #'A
-      BNE BORRA
-      STAA U3     * VALIDA A
-      JMP CICLATE
+      CBA
+      BEQ ERROR
       
-SIGUER
-      LDAB U4
-      BNE  SIGUEF
-      CMPA #'R
-      BNE BORRA
-      STAA U4     * VALIDA R
-      JMP CICLATE
+      CLR U3
+      JSR LONGITUD
 
-SIGUEF
-      CMPA #'T
-      BNE BORRA
-EXITO
-      LDAA #'E
-      STAA ORDEN
-      
-      JMP EXITO
+      LDAA U3
+      BNE ERROR
 
+      JMP ESPERAOK
+
+ERROR
+      JSR ESCRIBEERROR
+
+ESPERAOK
+      JSR ESPERAOK1
+      JMP START
+
+***********************************
+* Configura puerto serial
+***********************************
 SERIAL
 
        LDD   #$302C  * CONFIGURA PUERTO SERIAL
@@ -104,7 +83,245 @@ SERIAL
        TAP
       RTS
 
+
+***********************************
+* Lectura de Datos
+***********************************
+ENTRADA
+      LDAA #'?
+      STAA ORDEN
+CICLO
+      LDAA ORDEN
+      CMPA #'?
+      BEQ  CICLO
+       
+      CLR U3
+      CLR U4
+
+      JSR ESDIGITO
+      JSR ESROMANO
+      JSR NOESNINGUNO
+
+      CMPA #'=
+      BNE ENTRADA
       
+      STAA DUMP,X
+      INX
+      RTS
+
+***********************************
+* Es digito?
+***********************************
+ESDIGITO
+      LDAA ORDEN
+      LDAB #'/
+      CBA            
+      BLT NOESDIGITO
+      LDAB #':
+      CBA
+      BGT NOESDIGITO
+      
+      LDAB #$1
+      STAB U1
+
+      LDAB #$1
+      STAB U3
+      
+      TAB
+      LDAA CONT
+      ADDA #$1
+      STAA CONT
+      TBA
+
+      STAA DUMP,X
+      INX
+
+NOESDIGITO
+      RTS
+
+
+***********************************
+* Es romano?
+***********************************
+ESROMANO
+      LDAA ORDEN
+      LDAB #'I
+      CBA            
+      BEQ SIESROMANO
+
+      LDAB #'V
+      CBA
+      BEQ SIESROMANO
+
+      LDAB #'X
+      CBA
+      BEQ SIESROMANO
+
+      LDAB #'L
+      CBA
+      BEQ SIESROMANO
+
+      LDAB #'C
+      CBA
+      BEQ SIESROMANO
+
+      LDAB #'D
+      CBA
+      BEQ SIESROMANO
+
+      LDAB #'M
+      CBA
+      BNE NOESROMANO
+
+SIESROMANO
+      LDAB #$1
+      STAB U2
+
+      LDAB #$1
+      STAB U4
+
+      TAB
+      LDAA CONT
+      ADDA #$1
+      STAA CONT
+      TBA
+
+      STAA DUMP,X
+      INX 
+
+NOESROMANO
+      RTS
+
+
+***********************************
+* Es una cosa que no es digito ni romano ni =?
+***********************************
+NOESNINGUNO
+      LDAA ORDEN
+      CMPA #'=
+      BEQ ESALGO
+
+      LDAA U3
+      CMPA #$1
+      BEQ ESALGO
+
+      LDAA U4
+      CMPA #$1
+      BEQ ESALGO
+  
+      LDAA #$1
+      STAA U1
+
+      LDAA #$1
+      STAA U2
+ESALGO
+
+      RTS
+
+***********************************
+* Comprueba longitud
+***********************************
+LONGITUD
+      LDAA U1
+      CMPA #$1
+      BEQ ESNUMERO1
+
+      LDAA U2
+      CMPA #$1
+      BEQ ESROMANO1
+
+ESNUMERO1
+      LDAA CONT
+      CMPA #$5
+      BLT FINLONGITUD
+      LDAA #$1
+      STAA U3
+
+ESROMANO1
+      LDAA CONT
+      CMPA #$16
+      BLT FINLONGITUD
+      LDAA #$1
+      STAA U3
+      
+FINLONGITUD
+      RTS
+
+***********************************
+* ESCRIBE ERROR
+***********************************
+ESCRIBEERROR
+      LDX #SCSR
+      LDAA SCSR
+      LDAA #'E
+      STAA   SCDR
+C1
+      BRCLR $00,X,#$80 C1
+
+      LDX #SCSR
+      LDAA SCSR
+      LDAA #'R
+      STAA   SCDR
+C2
+      BRCLR $00,X,#$80 C2
+
+      LDX #SCSR
+      LDAA SCSR
+      LDAA #'R
+      STAA   SCDR
+C3
+      BRCLR $00,X,#$80 C3
+
+
+      LDX #SCSR
+      LDAA SCSR
+      LDAA #'O
+      STAA   SCDR
+C4
+      BRCLR $00,X,#$80 C4
+
+      LDX #SCSR
+      LDAA SCSR
+      LDAA #'R
+      STAA   SCDR
+C5
+      BRCLR $00,X,#$80 C5
+      RTS
+
+***********************************
+* ESPERAOK
+***********************************
+
+ESPERAOK1
+
+      CLR U1
+
+LOOP1
+      LDAA #'?
+      STAA ORDEN
+CICLO1
+      LDAA ORDEN
+      CMPA #'?
+      BEQ  CICLO1
+      
+      LDAA U1
+      BNE SIGUEK    * Ya vimos O
+      
+      LDAA ORDEN    * No hemos visto O
+      CMPA #'O
+      BNE ESPERAOK1 * No es O
+     
+      LDAA #$1      * Si es O, bandera 1 prendida
+      STAA U1
+      JMP LOOP1
+     
+SIGUEK
+
+      LDAA ORDEN
+      CMPA #'K
+      BNE ESPERAOK1
+
+      RTS
 ***********************************
 * ATENCION A INTERRUPCION SERIAL
 ***********************************
